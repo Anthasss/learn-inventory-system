089218bae5f8ae52525c3598dffa9e234e3b0f51
= <=>
2026-06-11 15:40:42 +0800

    added dummy endpoint to fetch data

---
+++ backend/src/dummy/dummyProducts.ts
@@ -0,0 +1,33 @@
+import type { Product } from "@inventory/shared";
+export const dummyProducts: Product[] = [
+  {
+    id: 1,
+    name: "Haha",
+    quantity: 33,
+    price: 2000,
+  },
+  {
+    id: 2,
+    name: "Hihi",
+    quantity: 33,
+    price: 2000,
+  },
+  {
+    id: 3,
+    name: "Huhu",
+    quantity: 33,
+    price: 2000,
+  },
+  {
+    id: 4,
+    name: "Hehe",
+    quantity: 33,
+    price: 2000,
+  },
+  {
+    id: 5,
+    name: "Hoho",
+    quantity: 33,
+    price: 2000,
+  },
+];
