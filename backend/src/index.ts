4dc5005fc46c2a3f1de57485360d759579344456
= <=>
2026-06-11 15:21:08 +0800

    added package.json

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,27 @@
 import express from "express";
+import type { Product } from "@inventory/shared";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
+const hehe: Product = {
+  id: 1,
+  name: "Hihi",
+  quantity: 33,
+  price: 2000,
+};
+
 app.get("/", (req, res) => {
   res.json({
-    message: "Hello TypeScript Express!",
+    data: "Ini data",
+    message: "Ini message",
   });
+});
+
+app.get("/haha", (req, res) => {
+  res.json(hehe);
 });
 
 app.listen(PORT, () => {
